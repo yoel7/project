1a2c5edb0e237713442fa298c1892ba85ebab096
yoel7 <76557026+yoel7@users.noreply.github.com>
2021-02-16 06:33:22 +0200

    עובד חוץ מבאג מוזר

--- src/app/card-coin/card-coin.component.ts
+++ src/app/card-coin/card-coin.component.ts
@@ -17,11 +17,9 @@ export class CardCoinComponent  {
   // checked:boolean=false;
   nonUpdated=false
   info='More';
-  // disabled=false;
   constructor(public apiCoingeckoService:ApiCoingeckoService) {}
   click(){
-    // this.checkedF();
-      this.visibility=(!this.visibility);
+    this.visibility=(!this.visibility);
     if(!this.visibility) {
         this.info='More';
       if (!this.cacheMore) this.more=undefined;
@@ -32,83 +30,31 @@ export class CardCoinComponent  {
     this.apiCoingeckoService.get(this.coin.id).subscribe((a)=>{
       this.more=a;
       this.cacheMore =true;this.nonUpdated=false;
-      
       setTimeout(() => {
-        this.cacheMore =undefined;console.log('קאש נמחק');
+        this.cacheMore =undefined;
       }, 3*1000);
     })
     setTimeout(()=>this.nonUpdated=true,3*1000)
   }
     addFaiv(){
-      var a = this.apiCoingeckoService.addFaiv(this.coin)
-      if (a==false) { console.log(this.checked,'a==false');
-        this.checked=false;
-        var b =window.confirm('אין אפשרות לבחור יותר מ-5 מטבעות, האם אתה מעוניין להסיר מטבע?');
-        // if(b) this.apiCoingeckoService.coins=this.apiCoingeckoService._coins.filter(a=>(a.add=true))
-        if(b) {
-          this.apiCoingeckoService.coins=this.apiCoingeckoService.coinsFaiv;console.log(this.checked,'checked -b=t');}
-        if (!b) {
-          this.checked=false;console.log(this.checked,'checked !b');this.constructor();}
-        // this.checkedF();
+      // debugger;
+      var a = this.apiCoingeckoService.addFaiv(this.coin.symbol)
+      if (a==false) {
+        var b=window.confirm('אין אפשרות לבחור יותר מ-5 מטבעות, האם אתה מעוניין להסיר מטבע?');
+        if(b)  {this.apiCoingeckoService.coins=this.apiCoingeckoService._coins.filter(a=>a.add);}
+        if (!b) {this.constructor(this.coin);}
       }
-    // this.apiCoingeckoService.update();
-    }
-    checkedF(){
-      this.checked=!this.checked; 
-    }
-    checkedHtml(a){
-      // return this.checked; 
-      console.log(a);
-      
     }
     myFunction(){
-      console.log(this.checked,'כניסה');
-      // debugger
-      //    version 1
+      console.log(
+       this.apiCoingeckoService._coins.filter(a=>a.add),'כניסה');
       this.checked=!this.checked;
-      // if (!this.checked) {
-      //     this.apiCoingeckoService.coins=this.apiCoingeckoService._coins;
-      //     this.apiCoingeckoService.deleteFavorites(this.coin);
-      // } else this.addFaiv()
-      //    version 2
       if (!this.checked) {
-        console.log(this.checked,'מחיקה');
-          this.apiCoingeckoService.coins=this.apiCoingeckoService._coins;
-          this.apiCoingeckoService.deleteFavorites(this.coin);
-      } else { this.addFaiv();console.log(this.checked,'add');}
-        // 
-
-    //   this.checkBox= document.getElementById("myCheck");
-    // // this.checkBox= .getElementById("customSwitches");
-    // if(this.checkBox.checked) this.addFaiv()
-    // else this.apiCoingeckoService.deleteFavorites(this.coin);
-    // else if (!this.checked) {
-    //   for(let i=0;i< this.co.length;i++){}
-    //   console.log(this.co);
-    // }
-    // else this.apiCoingeckoService.coinsFaiv.splice(this.coin);console.log(this.apiCoingeckoService.coinsFaiv);
-    // }
-    // this.checked=this.checkBox.checked;
-    // this.apiCoingeckoService.update();
-    // this.emit.emit(true)
+         this.apiCoingeckoService.coins=this.apiCoingeckoService._coins;
+          this.apiCoingeckoService.deleteFavorites(this.coin.symbol);
+      } else this.addFaiv();
+      console.log(
+        this.apiCoingeckoService._coins.filter(a=>a.add),'יציאה');
+       
     }
 }
-// this.a.addEventListener("click",this.click)
-
- // document.getElementById('raedMore').setAttribute("style", "display:none");
-    // document.getElementById('raedMore').style.visibility='visible';
-    // this.elementRef.nativeElement.
-    // console.log('hag',this.elementRef);
-    // if(this.visibility) {this.visibility=false}
-    // else this.visibility=true;
-
-    // st(){
-  //   if(this.visibility) return 'visible'
-  //   else return 'hidden';
-  // }
-
-
-   // public isCollapsed = true;
-  // display:string='none'
-
-  // a=document.getElementById("button")
